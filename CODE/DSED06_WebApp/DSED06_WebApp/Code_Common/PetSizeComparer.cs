73d57d425833fa475d6edf55e0d5ce92bdd8d8dc
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-07-01 19:51:21 +1200

    create LINQ extension

--- CODE/DSED06_WebApp/DSED06_WebApp/Code_Common/PetSizeComparer.cs
+++ CODE/DSED06_WebApp/DSED06_WebApp/Code_Common/PetSizeComparer.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text.RegularExpressions;
 
 namespace DSED06_WebApp.Code_Common
 {
-    public class SemiNumericComparer : IComparer<string>
+    public class PetSizeComparer : IComparer<string>
     {
+
+        private static string getNumeric(string input)
+        {
+            return new string(input.ToCharArray().Where(c => char.IsDigit(c)).ToArray());
+        }
         public int Compare(string s1, string s2)
         {
             if (IsNumeric(s1) && IsNumeric(s2))
             {
+                /*
                 if (Convert.ToInt32(s1) > Convert.ToInt32(s2)) return 1;
                 if (Convert.ToInt32(s1) < Convert.ToInt32(s2)) return -1;
                 if (Convert.ToInt32(s1) == Convert.ToInt32(s2)) return 0;
+                */
+                if (Convert.ToInt32(getNumeric(s1)) > Convert.ToInt32(getNumeric(s2))) return 1;
+                if (Convert.ToInt32(getNumeric(s1)) < Convert.ToInt32(getNumeric(s2))) return -1;
+                if (Convert.ToInt32(getNumeric(s1)) == Convert.ToInt32(getNumeric(s2)))
+                {
+                    return string.Compare(s1, s2);
+                    //return 0;
+                }
+
             }
 
             if (IsNumeric(s1) && !IsNumeric(s2))
@@ -25,16 +41,17 @@ namespace DSED06_WebApp.Code_Common
             return string.Compare(s1, s2, true);
         }
 
+
         public static bool IsNumeric(object value)
         {
             try
             {
-                //int i = Convert.ToInt32(value.ToString());
-                int i;
-                if (int.TryParse(value.ToString(), out i))
-                    return true;
-                else
-                    return false;
+                //string output = new string(value.ToString().ToCharArray().Where(c => char.IsDigit(c)).ToArray());
+
+                int i = Convert.ToInt32(getNumeric(value.ToString()));
+
+                return true;
+
             }
             catch (FormatException)
             {
